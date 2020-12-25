423d5a0e548f8f4e896d1953363e23cced478aa8
Carlos Sánchez <sanchez.carlos.1986@gmail.com>
2020-12-25 15:50:58 -0300

    feat(api): getting all meals, one by id, posting a new one, updating and deleting

--- api/routes/meals.js
+++ api/routes/meals.js
@@ -1,47 +1,53 @@
 const express = require("express");
 const Meals = require("../models/Meals");
 
+const colorLog = require("../../utils/colorLog");
+
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
   Meals.find()
-    .exec()
     .then((data) => {
-      next();
-      console.log("data:", data);
       res.status(200).send(data);
+      next();
     })
-    .catch((err) => console.log(`Error getting meals: ${err}`));
+    .catch((err) => colorLog("error", `Error getting meals: ${err}`));
 });
 
 router.get("/:id", (req, res, next) => {
   Meals.findById(req.params.id)
-    .exec()
-    .then((data) => res.status(200).send(data))
-    .catch((err) => console.log(`Error getting 1 meal: ${err}`));
+    .then((data) => {
+      res.status(200).send(data);
+      next();
+    })
+    .catch((err) => colorLog("error", `Error getting 1 meal: ${err}`));
 });
 
 router.post("/", (req, res, next) => {
   Meals.create(req.body)
     .then((data) => {
-      next();
       res.status(201).send(data);
+      next();
     })
-    .catch((err) => console.log(`Error posting meal: ${err}`));
+    .catch((err) => colorLog("error", `Error posting meal: ${err}`));
 });
 
-// router.put("/:id", (req, res, next) => {
-//   Meals.findOneAndUpdate(req.params.id, req.body)
-//     .exec()
-//     .then(() => res.status(204))
-//     .catch((err) => console.log(`Error updating meal: ${err}`));
-// });
+router.put("/:id", (req, res, next) => {
+  Meals.findOneAndUpdate(req.params.id, req.body)
+    .then(() => {
+      res.status(204);
+      next();
+    })
+    .catch((err) => colorLog("error", `Error updating meal: ${err}`));
+});
 
-// router.delete("/:id", (req, res, next) => {
-//   Meals.findOneAndDelete(req.params.id)
-//     .exec()
-//     .then(() => res.status(204))
-//     .catch((err) => console.log(`Error deleting meal: ${err}`));
-// });
+router.delete("/:id", (req, res, next) => {
+  Meals.findOneAndDelete(req.params.id)
+    .then(() => {
+      colorLog("info", `Meal id: "${req.params.id}" was deleted`);
+      next();
+    })
+    .catch((err) => colorLog("error", `Error deleting meal: ${err}`));
+});
 
 module.exports = router;
